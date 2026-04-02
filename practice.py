06cbacf3d89b2313016e871d10301f75f0d5e7ef
Dylan Tang <dtang04@uchicago.edu>
2026-04-02 00:06:20 -0500

    Updated df practice

--- practice.py
+++ practice.py
@@ -19,6 +19,7 @@ print("--------------")
 df_ind = df[df["department"] == "Engineering"]
 df_ind = df_ind[df_ind["salary"] > 70000]["name"]
 print(df_ind)
+print("")
 
 # Exercise 3
 data["bonus"] = np.array(data["salary"]) * 0.10
@@ -26,7 +27,46 @@ data["bonus"] = np.array(data["salary"]) * 0.10
 # Exercise 4
 df_salary = df.groupby(["department"])["salary"].mean()
 print(df_salary)
+print("")
 
 # Exercise 5
 df_sorted = df.sort_values(by = ["salary", "name"], ascending = [False, False])
 print(df_sorted)
+print("")
+
+# Exercise 6
+print(df.count()["name"])
+
+# Exercise 7
+
+df["hire_date"] = pd.to_datetime(df["hire_date"])
+
+def classify_senior(i):
+    if i.year < 2020:
+        return "Senior"
+    else:
+        return "Junior"
+
+df["seniority"] = df["hire_date"].apply(classify_senior)
+
+print(df["seniority"])
+
+# Exercise 8
+
+df_stats = df.groupby(["department"])["salary"].agg(["min", "max", "mean"])
+print(df_stats)
+
+# Exercise 9
+
+df_with_raise = df.copy()
+df_with_raise["salary"] = df_with_raise["salary"] + 5000
+
+# Exercise 10
+
+budgets = pd.DataFrame({
+    'department': ['Sales', 'Engineering', 'Marketing'],
+    'budget': [200000, 500000, 150000]
+})
+
+df_merged = pd.merge(df, budgets, on = "department")
+print(df_merged)
