861f3bab4ef458eb4ee4490a84f008bbf1ff5171
Dylan Tang <dtang04@uchicago.edu>
2026-04-30 18:23:52 -0500

    Added r calculation, predict

--- ml_practice/sunspots.py
+++ ml_practice/sunspots.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import TimeSeriesSplit
 
 from sklearn.linear_model import LinearRegression
 
@@ -34,10 +36,31 @@ def main():
     plt.scatter(df["Month_No"], df["Sunspots"])
     plt.show()
 
-    model = buildLinearRegressor(X_train_n, y_train)
-    print("AR(1) Model R^2: ", model.score(X_test_n, y_test_n))
+    # Should not use train_test_split on AR(1) models, as it does not respect time order (data leakage)
+    # model = buildLinearRegressor(X_train_n, y_train)
+    # print("AR(1) Model R^2: ", model.score(X_test_n, y_test_n))
 
+    model_tscv = None
+    fold = 1
+    tscv = TimeSeriesSplit(n_splits=5) # trains on past data and tests on future data, five-fold split
+    for train_idx, test_idx in tscv.split(X):
+        X_train_fold, X_test_fold = X_new.iloc[train_idx], X_new.iloc[test_idx]
+        y_train_fold, y_test_fold = y.iloc[train_idx], y.iloc[test_idx]
 
+        model_tscv = buildLinearRegressor(X_train_fold, y_train_fold)
+        r2 = model_tscv.score(X_test_fold, y_test_fold)
+
+        y_pred = model_tscv.predict(X_test_fold)
+        r = np.corrcoef(y_test_fold, y_pred)[0,1] #Returns [0,1]th element of correlation matrix
+
+        print(f"AR(1) Model fold = {fold}, R = {r}, R^2 = {r2}")
+
+        fold += 1
+    
+    # New data
+    X_new_pts = pd.DataFrame({"Month_No": [5, 3, 2], "prev_Sunspot": [100, 30, 75]})
+    print(model_tscv.predict(X_new_pts))
+    
 if __name__ == "__main__":
     main()
 
