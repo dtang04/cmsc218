0dd45a6078aae0df710b5e4ef1c89b56e4e8c9de
Dylan Tang <dtang04@uchicago.edu>
2026-04-30 15:56:26 -0500

    Added ridge regression, polynomial feature space

--- ml_practice/concrete.py
+++ ml_practice/concrete.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 from ucimlrepo import fetch_ucirepo 
+
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+
 from sklearn.model_selection import train_test_split
 
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 
 import matplotlib.pyplot as plt
 
@@ -20,16 +24,21 @@ X_norm = pd.DataFrame(scaler.fit_transform(X), columns = X.columns)
 # 80/20 split using train_test_split on X_norm
 X_train, X_test, y_train, y_test = train_test_split(X_norm, y, test_size = 0.2, random_state=1)
 
-def build_linear_regressor():
+def build_linear_regressor(X_, y_):
     """
     Build a naive linear regressor.
     """
     model = LinearRegression()
-    model.fit(X_train,y_train)
+    model.fit(X_, y_)
+    return model
+
+def build_ridge(X_, y_, reg = 0):
+    model = Ridge(alpha = reg)
+    model.fit(X_,y_)
     return model
 
 def main():
-    model = build_linear_regressor()
+    model = build_linear_regressor(X, y)
 
     # R^2 score of naive linear regressor
     print("R^2 score of linear regressor: ", model.score(X_test, y_test))
@@ -58,6 +67,19 @@ def main():
 
     plt.scatter(X_norm_adj["Superplasticizer"], y_adj)
     plt.show()
+
+    ridge_model = build_ridge(X_adj_train, y_adj_train, 5)
+    print("R^2 score of ridge regressior: ", ridge_model.score(X_adj_test, y_adj_test))
+
+    # Polynomial feature space
+    poly = PolynomialFeatures(degree=3)
+    X_poly = poly.fit_transform(X_norm_adj)
+
+    X_poly_train, X_poly_test, y_poly_train, y_poly_test = train_test_split(X_poly, y_adj, random_state=1)
+
+    # Build ridge regression model using X_poly
+    poly_ridge_model = build_ridge(X_poly_train, y_poly_train, 5)
+    print("R^2 score of ridge regressor trained on poly feature space: ", poly_ridge_model.score(X_poly_test, y_poly_test))
 
 if __name__ == "__main__":
     main()
