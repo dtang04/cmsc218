0a4613c4f88868e030ebe71a8f46e83a34c2d999
Dylan Tang <dtang04@uchicago.edu>
2026-05-17 17:44:44 -0500

    Added NN

--- ml_practice_2/practice.py
+++ ml_practice_2/practice.py
@@ -11,6 +11,8 @@ from sklearn.decomposition import PCA
 
 from sklearn.metrics import confusion_matrix, precision_score, accuracy_score
 
+import tensorflow as tf
+
 np.random.seed(42)
 
 def main():
@@ -48,7 +50,22 @@ def main():
     plt.ylabel('PC2')
     plt.legend()
     plt.title('PCA: Churned vs Stayed')
-    plt.show()
+    plt.show(block=False)
+
+    # ----Neural Network----
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.InputLayer(input_shape=(X.shape[1],)))
+    model.add(tf.keras.layers.Dense(64, activation='relu')) # relu for hidden layers prevent vanishing gradients
+    model.add(tf.keras.layers.Dense(32, activation='relu'))
+    model.add(tf.keras.layers.Dense(1, activation='sigmoid')) #logistic regression (binary classification)
+
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+    model.fit(X_train, y_train, epochs=50) # 50 forward + backprop cycles
+
+    loss, acc = model.evaluate(X_test, y_test)
+
+    print(loss, acc)
 
 if __name__ == "__main__":
     main()
